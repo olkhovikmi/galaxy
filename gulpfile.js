b8528c269605cb16c8e29e557e469e31a8a42638
olkhovikmi <olkhovikmi3@gmail.com>
2020-05-04 23:37:55 +0300

    шоблон gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ let path = {
 		fonts: project_folder + '/fonts/'
 	}, // пути вывода
 	src: {
-		html: source_folder + '/*.html',
+		html: [source_folder + '/*.html', '!' + source_folder + '/_*.html'],
 		css: source_folder + '/scss/style.scss',
 		js: source_folder + '/js/script.js',
 		img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
@@ -24,10 +24,17 @@ let path = {
 	},
 	clean: './' + project_folder + '/'
 };
-
+//ПЕРЕМЕННЫЕ
 let { src, dest } = require('gulp'),
 	gulp = require('gulp'),
-	browsersync = require('browser-sync').create();
+	browsersync = require('browser-sync').create(),
+	fileinclude = require('gulp-file-include'),
+	del = require('del'),
+	scss = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	group_media = require('gulp-group-css-media-queries'),
+	clean_css = require('gulp-clean-css'),
+	rename = require('gulp-rename');
 
 function browserSync() {
 	browsersync.init({
@@ -41,13 +48,49 @@ function browserSync() {
 
 function html() {
 	return src(path.src.html)
+		.pipe(fileinclude())
 		.pipe(dest(path.build.html))
 		.pipe(browsersync.stream());
-}
+} //работа с html файлами
 
-let build = gulp.series(html);
-let watch = gulp.parallel(build, browserSync);
+function css() {
+	return src(path.src.css)
+		.pipe(
+			scss({
+				outputStyle: 'expanded' // scss формируетя не сжатым
+			})
+		)
+		.pipe(group_media())
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ['last 5 versions'],
+				cascade: true
+			})
+		) // настройка аутопрефикса для css
+		.pipe(dest(path.build.css)) // выгружаем файл css
+		.pipe(clean_css()) //сжимаем css файл
+		.pipe(
+			rename({
+				extname: '.min.css'
+			})
+		) //  создаем сжатый файл css
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream());
+} //работа с css файлами
 
+function watchFiles() {
+	gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.css], css);
+} //обновляет код html
+
+function clear() {
+	return del(path.clean);
+} //удаляет папку dist
+
+let build = gulp.series(clear, gulp.parallel(css, html));
+let watch = gulp.parallel(build, watchFiles, browserSync); //вызывает функции
+
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
